08235751ac8d89c7a43243d5f906ace777078308
Franco Buceta <francobuceta95@gmail.com>
2023-05-01 14:52:23 -0300

    add faker methods

--- src/routes/mocking.router.js
+++ src/routes/mocking.router.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
+import { faker } from "@faker-js/faker";
 
 const router = Router();
 
+const createProducts = () => {
+    const product = {
+        id: faker.database.mongodbObjectId(),
+        title: faker.commerce.product(),
+        description: faker.commerce.productDescription(),
+        price: faker.commerce.price(),
+        code: faker.datatype.string(5),
+        stock: faker.datatype.number({ max: 100 }),
+        category: faker.commerce.department(),
+    }
+}
+
 router.get("/mockingproducts", (req, res) => {
-    
+
 })
