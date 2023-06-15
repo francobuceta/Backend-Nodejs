36a2d76df6b0a1ecc13d51912ed1c436308cd32a
Franco Buceta <francobuceta95@gmail.com>
2023-06-15 14:44:10 -0300

    add again the older url for swagger config

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -17,7 +17,7 @@ const options = {
             description: "My API Documentation",
         },
     ],
-    apis: [`./docs/**/*.yaml`],
+    apis: [`${__dirname}/docs/**/*.yaml`],
 };
 
 export const swaggerSetup = swaggerJSDoc(options);
