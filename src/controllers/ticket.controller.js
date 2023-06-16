29ebca9f58b730d5d24de3f343eec8c7cfc5aa67
Franco Buceta <francobuceta95@gmail.com>
2023-06-16 20:38:40 -0300

    add try catch in create ticket

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -4,9 +4,13 @@ class TicketController {
     createTicket = async (req, res) => {
         const { email } = req.user;
         const amount = res.locals.data;
-    
-        const newTicket = ticketService.createTicket(email, amount);
-        return newTicket;
+
+        try {
+            const newTicket = await ticketService.createTicket(email, amount);
+            return newTicket;
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
