35de71a97e90e31220d7446155232700007b9266
Franco Buceta <francobuceta95@gmail.com>
2023-07-14 10:35:01 -0300

    test login github without res.json

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -56,7 +56,7 @@ class UserController {
         const user = req.user;
 
         const token = generateToken(user);
-        return res.cookie("token", token, { httpOnly: true }).json({ messsage:"Ingreso exitoso", token }).redirect("http://localhost:3000");
+        return res.cookie("token", token, { httpOnly: true }).redirect("http://localhost:3000");
     }
 
     loginCurrent = async (req, res) => {
