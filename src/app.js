95c7832cea92361761a01e1adf6acb0f76107bc7
Franco Buceta <francobuceta95@gmail.com>
2023-07-14 19:57:01 -0300

    fix route in cors origin

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(cors({
-    origin: 'https://localhost:3000'
+    origin: 'http://localhost:3000'
 }));
 
 //Redireccionamiento a los archivos
