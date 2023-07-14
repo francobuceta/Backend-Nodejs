dc00ce9d0fcfc5689ea93b2ca80d4bf39b2cff2d
Franco Buceta <francobuceta95@gmail.com>
2023-07-14 14:39:33 -0300

    add credentials to cors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 //Configuracion inicial express
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-app.use(cors());
+app.use(cors({credentials:true}));
 
 //Redireccionamiento a los archivos
 app.use("/api/products", ProductsRouter.getRouter());
