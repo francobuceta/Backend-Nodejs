1bb29b6fb60c02e7b1bc48e169612cd62730567c
Franco Buceta <francobuceta95@gmail.com>
2023-07-15 11:13:45 -0300

    add google strategy

--- src/passport/passportStrategies.js
+++ src/passport/passportStrategies.js
@@ -1,6 +1,7 @@
 import { userModel } from "../dao/models/user.model.js";
 import passport from "passport";
 import { Strategy as GitHubStrategy } from "passport-github2";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt";
 import config from "../config/config.js";
 import cartService from "../services/cart.services.js";
@@ -31,6 +32,31 @@ passport.use("github", new GitHubStrategy({
     }
 ));
 
+//Google Strategy
+passport.use("google", new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.GOOGLE_CLIENT_CB
+},
+    async (accessToken, refreshToken, profile, done) => {
+        const user = await userModel.findOne({ email: profile._json.email });
+
+        if (!user) {
+            const newCart = await cartService.createCart({});
+            const newUser = {
+                firstName: profile._json.given_name,
+                lastName: profile._json.family_name || " ",
+                email: profile._json.email,
+                password: " ",
+                cart: {cartId: newCart._id}
+            }
+            const userDB = await userModel.create(newUser);
+            done(null, userDB);
+        } else {
+            done(null, user);
+        }
+    }
+));
 
 const cookieExtractor = (req) => {
     const token = req.cookies.token;
@@ -39,8 +65,8 @@ const cookieExtractor = (req) => {
 
 //JWT Strategy
 passport.use("jwt", new JwtStrategy({
-    //jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]), 
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]), 
+    //jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: "secretJWT"
 }, async (jwt_payload, done) => {
     done(null, jwt_payload.user);
