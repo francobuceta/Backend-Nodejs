f806f0f29163994e5b63f606e04acfe5a5e69b20
Franco Buceta <francobuceta95@gmail.com>
2024-01-12 19:48:17 -0300

    fix amount data in ticket controller

--- src/controllers/ticket.controller.js
+++ src/controllers/ticket.controller.js
@@ -3,10 +3,10 @@ import ticketService from "../services/ticket.services.js";
 class TicketController {
     createTicket = async (req, res) => {
         const { email } = req.user;
-        const amount = res.locals.data;
+        const purchaseData = req.body.purchaseInfo;
 
         try {
-            const newTicket = await ticketService.createTicket(email, amount);
+            const newTicket = await ticketService.createTicket(email, purchaseData.amount);
             return newTicket;
         } catch (error) {
             console.log(error);
