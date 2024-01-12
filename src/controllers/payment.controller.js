f20a4d5f3bcba0553d8fb770415cf384e7f52990
Franco Buceta <francobuceta95@gmail.com>
2024-01-12 19:48:53 -0300

    delete res.locals.data in payment controller

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,15 +1,11 @@
 import PaymentService from "../services/payment.services.js";
 
 class PaymentController {
-    createPayment = async (req, res, next) => {
+    createPayment = async (req, res) => {
         const purchaseData = req.body.purchaseInfo;
         try {
             const response = await PaymentService.createPaymentStripe(purchaseData);
             res.json({ message: "Pago realizado con éxito", payload: response });
-
-            //Guardamos el total en locals.data para que lo reciba el ticket controller.
-            res.locals.data = purchaseData.amount;
-            next();
         } catch (error) {
             throw error;
         }
