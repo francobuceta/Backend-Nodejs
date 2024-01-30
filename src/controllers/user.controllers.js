4e229c2be2fa4a062b8ad6361061ded1ee984db9
Franco Buceta <francobuceta95@gmail.com>
2024-01-30 18:18:41 -0300

    fix new user template email

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,12 +17,72 @@ class UserController {
         try {
             const newUser = await userService.createUser(req.body);
 
+            let cuerpoCorreo = `<!DOCTYPE html>
+                <html lang="es">
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>Cyber Cube</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                                text-align: center;
+                            }
+                            .container {
+                                max-width: 600px;
+                                margin: 50px auto;
+                                padding: 20px;
+                            }
+                            .logo {
+                                text-align: center;
+                                margin-bottom: 20px;
+                            }
+                            .title {
+                                font-size: 24px;
+                                margin-bottom: 20px;
+                                font-weight: bold;
+                            }
+                            .text {
+                                font-size: 18px;
+                                margin-bottom: 20px;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="container">
+                            <div class="logo">
+                                <img src="https://res.cloudinary.com/dzm5lgpyv/image/upload/v1704984217/cyber%20cube%20backend%20ecommerce/cybercube_logo_ivjjnt.png" 
+                                    alt="Logo de la empresa" width="150" height="auto"
+                                >
+                            </div>
+            
+                            <div class="title">
+                                ¡Hola ${newUser.firstName}!
+                            </div>
+            
+                            <div class="text">
+                                Muchas gracias por registrarte en Cyber Cube.
+                            </div>
+                            <div class="text">
+                                Te damos la bienvenida y esperamos poder ayudarte en lo que andas buscando.
+                            </div>
+                            <div style="text-align:center">
+                                <a href="https://ecommerce-next-js-ebon.vercel.app/" target="_blank">
+                                    <button style="border: none;width: 100%;max-width: 260px;height: 40px;background-color: #BAFF29;border-radius: 18px;font-size: 16px;font-weight: 700;cursor: pointer;">
+                                        IR AL SITIO
+                                    </button>
+                                <a>
+                            </div>
+                        </div>
+                    </body>
+                </html>
+            `;
+
             if (newUser) {
                 await transporter.sendMail({  //Envio de email al usuario si pudo registrarse correctamente
-                    from: "E-commerce",
+                    from: '"Cyber Cube" <cybercube_soporte@gmail.com>',
                     to: newUser.email,
-                    subject: "Usuario Nuevo",
-                    text: `Gracias por registrarte en nuestro sitio ${newUser.firstName}`
+                    subject: "Cuenta creada con éxito",
+                    html: cuerpoCorreo
                 });
                 res.json({ message:"Usuario creado con éxito" });
             } else {
